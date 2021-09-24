13ab15f96d7a40803bbbfa1701a1d1dcc5ba5eb4
Rafael Hernandez <rafael@koalaworkshop.com>
2021-09-24 14:01:58 -0500

    Complete tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -23,3 +23,11 @@ it('can scope by type moral', function () {
     expect((new RFC(RFC::PERSONA_MORAL))->passes('test', 'XAX010101000'))->toBeTrue();
     expect((new RFC(RFC::PERSONA_MORAL))->passes('test', 'XAXX010101000'))->toBeFalse();
 });
+
+it('returns generic message', function (){
+    expect((new RFC())->message())->toBe('El RFC no tiene un formato válido.');
+});
+
+it('returns specific message', function (){
+    expect((new RFC(RFC::PERSONA_MORAL))->message())->toBe('El RFC no es un formato válido para persona moral.');
+});
